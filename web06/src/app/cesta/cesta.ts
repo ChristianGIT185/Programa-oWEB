342b907a0b6ff1f3b06af08bcecbbfd3c07bdd73
ChristianGIT185 <moiak900@gmail.com>
2026-09-23 18:26:55 -0300

    Revert "Implementa cesta de compras no web06"
    This reverts commit ed7190eb7a07010bb80ce4862d85f7eda499e5b3.

--- web06/src/app/cesta/cesta.ts
+++ web06/src/app/cesta/cesta.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ItemCesta } from '../model/item-cesta';
 
 @Component({
   imports: [CommonModule],
@@ -9,14 +8,24 @@ import { ItemCesta } from '../model/item-cesta';
   templateUrl: './cesta.html',
 })
 export class Cesta {
-  lista: ItemCesta[] = [];
-
-  ngOnInit(): void {
-    if (typeof localStorage === 'undefined') {
-      return;
-    }
-
-    const cestaSalva = localStorage.getItem('cesta');
-    this.lista = cestaSalva ? JSON.parse(cestaSalva) : [];
-  }
+  lista = [
+    {
+      codigo: 1,
+      nome: 'Martelo de Unha 25mm',
+      quantidade: 2,
+      valor: 39.9,
+    },
+    {
+      codigo: 3,
+      nome: 'Jogo de Chaves Allen',
+      quantidade: 1,
+      valor: 32.9,
+    },
+    {
+      codigo: 8,
+      nome: 'Furadeira de Impacto 650W',
+      quantidade: 1,
+      valor: 289.9,
+    },
+  ];
 }
